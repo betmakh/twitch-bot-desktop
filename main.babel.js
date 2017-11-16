eb2c55ca6ee91e3faa84f839d66cea93213b1080
Mykyta Meshcheriakov <mmeshcheriakov@gmail.com>
2017-11-16 16:52:50 +0200

    trash removed

--- main.babel.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _appMainProcess = require('./src/appMainProcess.js');
-
-var _appMainProcess2 = _interopRequireDefault(_appMainProcess);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
